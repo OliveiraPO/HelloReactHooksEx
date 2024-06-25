0e88dee0b0380889a43b01cdb4e8a6ec8b08bcd1
Vinicius Paulo de Oliveira <viniciusp.oliveirapo@gmail.com>
2024-06-24 23:47:24 -0300

    atualizado

--- HelloReactHooks/src/pages/freestyle/Freestyle.tsx
+++ HelloReactHooks/src/pages/freestyle/Freestyle.tsx
@@ -1,25 +1,17 @@
 import React, { ReactElement, ReactNode, useEffect, useState } from "react"
 
-const displayPedidos = (pedidos: any) : ReactNode => {
-  const result : any = []
-  if(pedidos){
-    pedidos.forEach((pedido: any) => {
-      result.push(
-       <h2 style={{ width: "200px", height: "200px", border: "1px solid black", borderRadius: "20px"}}>
-        {pedido.restaurante}
-        <br />
-        {pedido.prato}
-        </h2>
-      )
-    })
-  }
-
-  return result
-}
-/*op*/
 const FreestylePage = (): ReactElement => {
   const [pedidos, setPedidos] = useState([])
-  const [flavorName, setFlavorName] = useState("")
+  const [restaurantName, setRestaurantName] = useState("")
+  const [pratoName, setPratoName] = useState("")
+  const [newId, setNewId] = useState("")
+  const [newIdDelete, setNewIdDelete] = useState("")
+  const [newQuantity, setNewQuantity] = useState(0)
+  const [restaurantNameForUpdate, setRestaurantNameForUpdate] = useState("");
+  const [pratoNameForUpdate, setPratoNameForUpdate] = useState("");
+  const [newQuantityForUpdate, setNewQuantityForUpdate] = useState(0);
+  const [idPedido, setIdPedido] = useState("");
+  const [pedidoEncontrado, setPedidoEncontrado] = useState(null);
 
   useEffect(() => {
     fetchData()
@@ -33,14 +25,14 @@ const FreestylePage = (): ReactElement => {
       setPedidos(data)
     })
   }
-
-  const submitData = async () => {
-
-    const id = "4c9e265c-0471-4f07-92d5-2538a7416167"
-    const restaurante = flavorName
-    const prato = "raviole"
-    const quantidade = 5
-
+  
+const alterarPedido = async () => {
+  
+    const id = newId
+    const restaurante = restaurantName
+    const prato = pratoName
+    const quantidade = newQuantity
+  
       await fetch(`http://localhost:4000/PedidosAtualiza/${id}?restaurante=${restaurante}&prato=${prato}&quantidade=${quantidade}`, {
         method: "PUT"
       })
@@ -50,17 +42,133 @@ const FreestylePage = (): ReactElement => {
         })
     }
 
+const cadastraPedido = async () => {
+
+  const restaurante = restaurantName
+  const prato = pratoName
+  const quantidade = newQuantity
+
+    await fetch(`http://localhost:4000/Pedidos/?restaurante=${restaurante}&prato=${prato}&quantidade=${quantidade}`, {
+      method: "POST"
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        fetchData()
+      })
+  }
+
+const listaPedidos = (pedidos: any) : ReactNode => {
+  const result : any = []
+  if(pedidos){
+    pedidos.forEach((pedido: any) => {
+      result.push(
+       <h2 style={{ width: "300px", height: "150px", border: "1px solid black", borderRadius: "20px", display: "flex", flexDirection: "column" }}>
+        <br />
+        Restaurante: {pedido.restaurante}
+        <br />
+        Prato: {pedido.prato}
+        <br />
+        Quantidade: {pedido.quantidade}
+        </h2>
+      )
+    })
+  }
+
+  return result
+}
+
+{/*const buscarPedidoPorId = async () => {
+    const response = await fetch(`http://localhost:4000/Pedidos/${idPedido}`);
+    if (response.ok) {
+      return(
+        <h2 style={{ width: "300px", height: "150px", border: "1px solid black", borderRadius: "20px", display: "flex", flexDirection: "column" }}>
+         <br />
+         Restaurante: {response.restaurante}
+         <br />
+         Prato: {pedido.prato}
+         <br />
+         Quantidade: {pedido.quantidade}
+         </h2>
+       )
+    }
+};
+    
+}*/}
+
+const deletePedido = async () => {
+  const id = newIdDelete
+
+  await fetch(`http://localhost:4000/PedidosDelete/${id}`,{
+    method: "DELETE"
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      fetchData()
+    })
+}
+
   return (
     <React.Fragment>
-      <h1>Título Legal</h1>
-      {displayPedidos(pedidos)}
-
-      <h1>{flavorName}</h1>
-      <input 
-        type="text"
-        value={flavorName}
-        onChange={(e) => {setFlavorName(e.target.value)}}/>
-      <button onClick={()=> {submitData()}}>Clique em mim!</button>
+      <div style={{display:"flex", flexWrap: "wrap", columnGap: "40px", width: "100%"}}>
+        <div style={{ maxWidth: "300px" }}>{/*LISTAR*/}
+          <h1>Lista de Pedidos</h1>
+          {listaPedidos(pedidos)}
+        </div>
+        <div style={{ maxWidth: "300px" }}>{/*UPDATE*/}
+          <h1>Alterar Pedido</h1>
+          <input 
+            type="text"
+            value={newId}
+            onChange={(e) => {setNewId(e.target.value)}} placeholder="Id Pedido" style={{padding: "5px", marginBottom: "5px", borderRadius: "7px", border: "1px solid black"}}/>
+          <br />
+          <input 
+            type="text"
+            value={restaurantNameForUpdate}
+            onChange={(e) => {setRestaurantNameForUpdate(e.target.value)}} placeholder="Nome Restaurante" style={{padding: "5px", marginBottom: "5px", borderRadius: "7px", border: "1px solid black"}}/>
+          <br />
+          <input 
+            type="text"
+            value={pratoNameForUpdate}
+            onChange={(e) => {setPratoNameForUpdate(e.target.value)}} placeholder="Nome do Prato" style={{padding: "5px", marginBottom: "5px", borderRadius: "7px", border: "1px solid black"}}/>
+          <br />
+          <input 
+            type="number"
+            onChange={(e) => {setNewQuantityForUpdate(parseInt(e.target.value))}} placeholder="Quantidade" style={{padding: "5px !important", marginBottom: "5px !importante", borderRadius: "7px", border: "1px solid black"}}
+            value={newQuantityForUpdate}/>
+          <br />
+          <button onClick={()=> {alterarPedido()}} style={{marginTop: "10px", padding:"5px", backgroundColor: "red", color: "white", fontSize:"18px", fontWeight: "bold", border: "1px solid rgb(136, 2, 2)", borderRadius:"5px" }}>Alterar</button>
+        </div>
+        <div style={{ maxWidth: "300px" }}>{/*DELETE*/}
+          <h1>Deletar Pedido</h1>
+          <input 
+            type="text"
+            value={newIdDelete}
+            onChange={(e) => {setNewIdDelete(e.target.value)}} placeholder="Id Pedido" style={{padding: "5px", marginBottom: "5px", borderRadius: "7px", border: "1px solid black"}}/>
+            <button onClick={()=> {deletePedido()}} style={{marginTop: "10px", padding:"5px", backgroundColor: "red", color: "white", fontSize:"18px", fontWeight: "bold", border: "1px solid rgb(136, 2, 2)", borderRadius:"5px" }}>Deletar</button>
+        </div>
+        <div style={{ maxWidth: "300px" }}>{/*CADASTRO*/}
+          <h1>Cadastrar Pedido</h1>
+          <input 
+            type="text"
+            value={restaurantName}
+            onChange={(e) => {setRestaurantName(e.target.value)}} placeholder="Nome Restaurante" style={{padding: "5px", marginBottom: "5px", borderRadius: "7px", border: "1px solid black"}}/>
+          <br />
+          <input 
+            type="text"
+            value={pratoName}
+            onChange={(e) => {setPratoName(e.target.value)}} placeholder="Nome do Prato" style={{padding: "5px", marginBottom: "5px", borderRadius: "7px", border: "1px solid black"}}/>
+          <br />
+          <input 
+            type="number"
+            onChange={(e) => {setNewQuantity(parseInt(e.target.value))}} placeholder="Quantidade" style={{padding: "5px !important", marginBottom: "5px !importante", borderRadius: "7px", border: "1px solid black"}}
+            value={newQuantity}/>
+          <br />
+          <button onClick={()=> {cadastraPedido()}} style={{marginTop: "10px", padding:"5px", backgroundColor: "red", color: "white", fontSize:"18px", fontWeight: "bold", border: "1px solid rgb(136, 2, 2)", borderRadius:"5px" }}>Cadastrar</button>
+        </div>
+        <div style={{ maxWidth: "300px" }}>{/* BUSCA POR ID */}
+          
+        </div>
+      </div>
     </React.Fragment>
   )
 }
